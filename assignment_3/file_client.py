67b93b3b7b51f072b3cf4c73b2b26d8837231650
KHjae <khj40286@naver.com>
2019-03-23 16:24:25 +0900

    client

--- assignment_3/file_client.py
+++ assignment_3/file_client.py
@@ -7,18 +7,22 @@ import os
 def run(host, port, Fname):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
-
+        
+        PATH = "C://Users//khj40//Cnetwork//assignment_3//" + Fname  # 전송받을 파일의 경로
         print("file name : %s " % (Fname))
-        s.sendall(Fname.encode())
-        path = "C://Users//khj40//Cnetwork//assignment_3//" + Fname  # 전송받을 파일의 경로 
-        pull_item = s.recv(1024) # 전송받은 파일
-        with open(Fname, "wb") as f:
-                f.write(pull_item) # 파일 저장
-        if os.path.getsize(path) == 0:
-                os.remove(Fname) # 생성된 파일 삭제
-                print("파일 요청 실패")
+        s.sendall(Fname.encode()) # [1]
+        msg = s.recv(1).decode() # 파일의 존재 여부 [2]
+        if msg == "Y" :
+                Fsize = s.recv(1024).decode() # 전송받은 파일의 크기 [3] 
+                s.sendall("OK".encode()) # [4]
+                pull_item = s.recv(int(Fsize)) # 파일의 사이즈만큼 서버로 부터 파일 수신 [5]
+                with open(Fname, "wb") as f:
+                        f.write(pull_item) # 파일 저장
+                print("file size : %d" % (os.path.getsize(PATH))) #전송 받은 파일의 크기        
         else:
-                print("file size : %d" % (os.path.getsize(path))) #전송 받은 파일의 크기
+                print("fail")
+        
+                
         
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Echo client -p port -i host -f Fname")
